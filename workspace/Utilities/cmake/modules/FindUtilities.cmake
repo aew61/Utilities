d49963d560ea9e3e4990b971d1b3241f3c42a47e
Andrew Wood <aew61@case.edu>
2016-08-02 12:38:48 -0400

    changing cmake imports

--- workspace/Utilities/cmake/modules/FindUtilities.cmake
+++ workspace/Utilities/cmake/modules/FindUtilities.cmake
@@ -12,7 +12,7 @@ if( ${CMAKE_SYSTEM_NAME} MATCHES "Windows" )
             ${CMAKE_INSTALL_PREFIX}/bin
             ${CMAKE_PREFIX_PATH}/bin
     )
-    find_library( UTILITIES_LIB Utilities
+    find_library( UTILITIES_LIB_IMPL Utilities
         HINTS
             ${CMAKE_INSTALL_PREFIX}/lib
             ${CMAKE_PREFIX_PATH}/lib
@@ -25,8 +25,23 @@ else()
     )
 endif()
 
-if( NOT UTILITIES_INCLUDES OR NOT UTILITIES_SHARED_LIB )
-    set( UTILITIES_FOUND FALSE )
-else()
+# make library target
+if( NOT TARGET UTILITIES_LIB )
+    add_library( UTILITIES_LIB SHARED IMPORTED )
+
+    # will set .so for unix systems and .dll for windows
+    set_property( TARGET UTILITIES_LIB PROPERTY
+        IMPORTED_LOCATION ${UTILITIES_SHARED_LIB} )
+
+    # need to link to .lib files for windows
+    if( ${CMAKE_SYSTEM_NAME} MATCHES "Windows" )
+        set_property( TARGET UTILITIES_LIB PROPERTY
+            IMPORTED_IMPLIB ${UTILITIES_LIB_IMPL} )
+    endif( ${CMAKE_SYSTEM_NAME} MATCHES "Windows" )
+endif()
+
+if( UTILITIES_INCLUDES AND UTILITIES_LIB )
     set( UTILITIES_FOUND TRUE )
+else()
+    set( UTILITIES_FOUND FALSE )
 endif()
